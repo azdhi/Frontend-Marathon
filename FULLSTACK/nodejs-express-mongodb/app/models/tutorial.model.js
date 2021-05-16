1f9f7956d0266cedd34537130fe5c515cef325a3
azdhi <azdhi93@gmail.com>
2021-05-16 09:29:52 +0700

    Revision

---
+++ FULLSTACK/nodejs-express-mongodb/app/models/tutorial.model.js
@@ -0,0 +1,16 @@
+const { mongoose } = require(".");
+
+module.exports = mongoose => {
+    const Tutorial = mongoose.model(
+        "tutorial",
+        mongoose.Schema(
+            {
+                title: String,
+                description: String,
+                published: Boolean
+            },
+            {timestamp: true}
+        )
+    );
+}
+return Tutorial;
